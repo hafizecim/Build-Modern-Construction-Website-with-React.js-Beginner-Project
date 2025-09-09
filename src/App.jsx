38249a6ed6b0c7bf8b8c99590de6481a0a449af6
hafizecim <hfz.snyl@gmail.com>
2025-09-09 20:39:12 +0300

    WhyChooseUs Page index.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Navbar from "./Sections/Navbar";
 import Hero from "./Sections/Hero";
 import About from "./Sections/About";
+import WhyChooseUs from "./Sections/WhyChooseUs";
+import "swiper/css"
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
-      <About /> 
+      <About />
+      <WhyChooseUs />
+      
     </>
   );
 }
