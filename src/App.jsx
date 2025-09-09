2354aef8cae29d320a888259cc8bde174e4072ba
hafizecim <hfz.snyl@gmail.com>
2025-09-09 19:36:15 +0300

    About page => index.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./Sections/Navbar";
 import Hero from "./Sections/Hero";
+import About from "./Sections/About";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <About /> 
     </>
   );
 }
